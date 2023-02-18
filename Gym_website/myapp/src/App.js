0ff9f4ab3f4ba1f57384e8475e98ed6606fbbab4
shahat <me6847570@gmail.com>
2023-02-18 13:28:26 +0200

    in this commit i finished the firest react project the gym website

---
+++ Gym_website/myapp/src/App.js
@@ -0,0 +1,31 @@
+
+import Footer from './Footer';
+import About from './About';
+import Blog from './Blog';
+import Carousel from './Carousel';
+import Facts from './Facts';
+import Header from './Header';
+import Program from './Program';
+import Team from './Team';
+import Testimonial from './Testimonial';
+import Timetable from './Timetable';
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <Carousel/>
+    <About/>
+    <Program/>
+    <Timetable/>
+    <Facts/>
+    <Team/>
+    <Testimonial/>
+    <Blog/>
+    <Footer/>
+    
+    </>
+ );
+}
+
+export default App;
